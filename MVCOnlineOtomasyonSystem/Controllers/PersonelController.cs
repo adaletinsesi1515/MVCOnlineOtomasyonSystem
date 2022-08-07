5419dc27408fecc7d855affa4dfb2e5ada60b5e2
adaletinsesi1515 <memduhgumen@gmail.com>
2022-08-07 23:51:51 +0300

    Personel added

---
+++ MVCOnlineOtomasyonSystem/Controllers/PersonelController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCOnlineOtomasyonSystem.Models.Siniflar;
+
+namespace MVCOnlineOtomasyonSystem.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            //Dropdown için oluşturuyoruz. 
+            List<SelectListItem> deger1 = (from x in c.Departmen.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
