93f94f81f17b346e6dcf793451a126b552361549
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2022-08-22 23:17:44 +0300

    75.ders sonu

---
+++ MVCOnlineOtomasyonSystem/Controllers/FaturaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCOnlineOtomasyonSystem.Models.Siniflar;
+
+
+namespace MVCOnlineOtomasyonSystem.Controllers
+{
+    public class FaturaController : Controller
+    {
+        Context c = new Context();
+        // GET: Fatura
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+    }
+}
--- MVCOnlineOtomasyonSystem/Models/Siniflar/Faturalar.cs
+++ MVCOnlineOtomasyonSystem/Models/Siniflar/Faturalar.cs
@@ -21,10 +21,14 @@ namespace MVCOnlineOtomasyonSystem.Models.Siniflar
         public string FaturaSiraNo { get; set; }
         public DateTime Tarih { get; set; }
 
+        [Column(TypeName = "Char")]
+        [StringLength(5)]
+        public string Saat { get; set; }
+
         [Column(TypeName = "varChar")]
         [StringLength(30)]
         public string VergiDairesi { get; set; }
-        public DateTime Saat { get; set; }
+       
 
         [Column(TypeName = "varChar")]
         [StringLength(30)]
